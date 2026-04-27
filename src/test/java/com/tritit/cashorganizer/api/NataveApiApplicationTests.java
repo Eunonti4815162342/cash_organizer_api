5038e224c69d6eef24d70c03d2124b43508f38a3
Álvaro Veiga Vázquez <alvaro.veiga@sngular.com>
2026-04-27 14:38:54 +0200

    feat

--- src/test/java/com/tritit/cashorganizer/api/NataveApiApplicationTests.java
+++ src/test/java/com/tritit/cashorganizer/api/NataveApiApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class CashOrganizerApiApplicationTests {
+class NataveApiApplicationTests {
 
 	@Test
 	void contextLoads() {
